e3acd6355a7435ac28d2d47167c42838535abe27
jassyio <joejassyio@gmail.com>
2023-08-29 10:17:27 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+
+"""This module defines a Square class"""
+
 class Square:
-    """Class Square that defines a square.
-    """
-    pass
+    """Square class with a private instance attribute"""
+    def __init__(self, size):
+        """Initializes the data"""
+        self.__size = size
