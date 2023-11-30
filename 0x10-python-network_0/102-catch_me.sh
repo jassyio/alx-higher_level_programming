cf29b7db8de3991fd41239b30ebda86da219e5c8
jassyio <joejassyio@gmail.com>
2023-11-30 16:34:21 +0300

    102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to causes an specific response
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "You got me!"
+curl -sL -X PUT -H "You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
