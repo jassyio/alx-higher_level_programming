b6a4b58df4bd34334f3bc474c6992ff191d10d4d
jassyio <joejassyio@gmail.com>
2023-12-01 13:14:31 +0300

    01

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """ Python script that fetches https://intranet.hbtn.io/status """
+
+
+
 if __name__ == "__main__":
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
@@ -7,4 +10,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))       
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
