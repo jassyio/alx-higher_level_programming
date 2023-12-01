d84b0b911a6cc680baa6f08eb226b1396b4b5450
jassyio <joejassyio@gmail.com>
2023-12-01 15:29:13 +0300

    001

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
+"""Python script that fetches https://intranet.hbtn.io/status"""
 import urllib.request
-
+import urllib.error
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
-        
+    url = "https://alx-intranet.hbtn.io/status"
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+    except urllib.error.URLError as e:
+        print("Error accessing the server: {}".format(e.reason))
