cf74f3c22daef4e1724fc19b747455e082829f8c
jassyio <joejassyio@gmail.com>
2023-12-01 15:47:18 +0300

    03

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,21 +8,8 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <URL>".format(sys.argv[0]))
-        sys.exit(1)
-
     url = sys.argv[1]
 
-    try:
-        request = urllib.request.Request(url)
-        with urllib.request.urlopen(request) as response:
-            x_request_id = dict(response.headers).get("X-Request-Id")
-            if x_request_id:
-                print(x_request_id)
-            else:
-                print("X-Request-Id header not found in the response.")
-    except urllib.error.URLError as e:
-        print("Error accessing the URL:", e.reason)
-    except urllib.error.HTTPError as e:
-        print("HTTP Error:", e.code)
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
