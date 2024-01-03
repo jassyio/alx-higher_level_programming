9bc5d01667d21edb8ecd430a003c7e7c637b36b1
jassyio <joejassyio@gmail.com>
2024-01-03 12:50:49 +0300

    war

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
+
 const request = require('request');
 
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(apiUrl, function(error, response, body) {
+request(apiUrl, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const movieData = JSON.parse(body);
     console.log(movieData.title);
